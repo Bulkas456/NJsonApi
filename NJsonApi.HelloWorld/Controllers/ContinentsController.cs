88bb73c35f17aeeaa7f2ae7620a530afc5fca428
Bartosz Bukowski <bartosz.bukowski@thetrainline.com>
2019-02-12 10:18:35 +0100

    HelloWorld project in net core.

---
+++ NJsonApi.HelloWorld/Controllers/ContinentsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NJsonApi.HelloWorld.Models;
+
+namespace NJsonApi.HelloWorld.Controllers
+{
+    [Route("continents")]
+    public class ContinentsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Continent> Get()
+        {
+            return StaticPersistentStore.Continents;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Continent Get(int id)
+        {
+            try
+            {
+                return StaticPersistentStore.Continents.Single(w => w.Id == id);
+            }
+            catch
+            {
+                throw new NotSupportedException($"Not supported for id: '{id}'");
+            }
+        }
+    }
+}
---
+++ NJsonApi.HelloWorld/Controllers/WorldsController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Linq;
+using NJsonApi.Common.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using NJsonApi.HelloWorld.Models;
+using System;
+
+namespace NJsonApi.HelloWorld.Controllers
+{
+    [Route("worlds")]
+    public class WorldsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<World> Get()
+        {
+            return StaticPersistentStore.Worlds;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public World Get([FromRoute]int id)
+        {
+            try
+            {
+                return StaticPersistentStore.Worlds.Single(w => w.Id == id);
+            }
+            catch
+            {
+                throw new NotSupportedException($"Not supported for id: '{id}'");
+            }
+        }
+
+        [HttpPost]
+        public World Post([FromBody]Delta<World> worldDelta)
+        {
+            var world = worldDelta.ToObject();
+            world.Id = StaticPersistentStore.Worlds.Max(w => w.Id) + 1;
+            StaticPersistentStore.Worlds.Add(world);
+            return world;
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public World Put([FromBody]Delta<World> worldDelta, [FromRoute]int id)
+        {
+            var world = Get(id);
+            worldDelta.Apply(world);
+            return world;
+        }
+    }
+}
