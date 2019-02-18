c86dbd08c580c85ebdc81f29a4c3547025c56071
Bartosz Bukowski <bartosz.bukowski@thetrainline.com>
2019-02-18 14:03:11 +0100

    Added ReadAsAsyncFromJsonApi extension.

---
+++ NJsonApi/Formatter/Input/HttpClient/HttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJsonApi.Formatter.Input.HttpClient
+{
+    public static class HttpClientExtensions
+    {
+        public static Task<TData> ReadAsAsyncFromJsonApi<TData>(this HttpContent httpContent, IJsonApiInputMapper mapper, params string[] supportedMediaTypes)
+        {
+            return httpContent.ReadAsAsync<TData>(new MediaTypeFormatter[]
+            {
+                new JsonApiMediaTypeFormatter<TData>(mapper, supportedMediaTypes)
+            });
+        }
+    }
+}
---
+++ NJsonApi/Formatter/Input/HttpClient/JsonApiMediaTypeFormatter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using NJsonApi.Serialization.Converters;
+using NJsonApi.Serialization.Documents;
+
+namespace NJsonApi.Formatter.Input.HttpClient
+{
+    public class JsonApiMediaTypeFormatter<TData> : BufferedMediaTypeFormatter
+    {
+        private readonly JsonSerializer jsonSerializer;
+        private readonly IJsonApiInputMapper mapper;
+
+        public JsonApiMediaTypeFormatter(IJsonApiInputMapper mapper, params string[] supportedMediaTypes)
+        {
+            this.mapper = mapper;
+            this.jsonSerializer = new JsonSerializer
+            {
+                Converters =
+                {
+                    new RelationshipConverter(),
+                    new ResourceConverter(),
+                    new LinkConverter(),
+                    new ResourceLinkageConverter(),
+                }
+            };
+
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue(Constants.JsonApiContentType));
+
+            foreach (string mediaType in supportedMediaTypes)
+            {
+                SupportedMediaTypes.Add(new MediaTypeHeaderValue(mediaType));
+            }
+
+            SupportedEncodings.Add(new UTF8Encoding(false, true));
+        }
+
+        public override object ReadFromStream(Type type, Stream readStream, HttpContent content, IFormatterLogger formatterLogger)
+        {
+            using (StreamReader reader = new StreamReader(readStream))
+            {
+                using (JsonTextReader jsonReader = new JsonTextReader(reader))
+                {
+                    CompoundDocument compoundDocument = this.jsonSerializer.Deserialize<CompoundDocument>(jsonReader);
+
+                    if (compoundDocument == null)
+                    {
+                        return null;
+                    }
+
+                    return this.mapper.Map(compoundDocument);
+                }
+            }
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return type == typeof(TData);
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return false;
+        }
+    }
+}
